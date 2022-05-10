e771193d2eb6a3024fa6accd977e390720876822
Ramziddin Makhmudov <ramziddin.makhmudov@gmail.com>
2022-05-10 21:16:31 +0500

    Change DB to Postgres

--- config/database.js
+++ config/database.js
@@ -1,11 +1,14 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: env("DATABASE_SSL", false),
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
